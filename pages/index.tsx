8a4ee1e97131aaff27ef924bb06016ae79fae5b1
hkmjtech <ajmlhkm@gmail.com>
2023-05-19 20:10:54 +0800

    ver num

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import SearchPalette from "@/components/SearchPalette";
+import packageJson from "../package.json";
 
 export default function Home() {
   return (
     <>
       <div>
         <SearchPalette></SearchPalette>
+        <div className="h-screen relative">
+          <div className="absolute bottom-0 right-0 m-4 text-gray-300 text-sm font-mono">
+            <span>Hakeem Jamal · v{packageJson.version}</span>
+          </div>
+        </div>
       </div>
     </>
   );
